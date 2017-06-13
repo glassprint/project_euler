a3f72d00334de149912fff0ee4eeac53ae30f6c1
Zach Weaver <r3dsierra@gmail.com>
2017-06-13 16:39:46 -0700

    add proper js file

--- project_euler.js
+++ project_euler.js
@@ -1,9 +1,15 @@
+// Euler problem 1
+// ===============
+
 function multiples(num) {
   var sum = 0;
   for (i = 0;i < num; i++) {
-    if(i / 3 === 0 || i / 5 === 0) {
+    if(i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
   return sum;
 }
+
+// Euler problem 2
+// ===============
